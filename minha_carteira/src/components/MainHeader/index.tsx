9639c752efd98df0e2c3c99673df91f262316da1
adriastephanie <adriastephanie@hotmail.com>
2024-05-13 21:05:04 -0300

    feature: criacao do emoji

--- minha_carteira/src/components/MainHeader/index.tsx
+++ minha_carteira/src/components/MainHeader/index.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { Container } from './styles';
+import React, {useMemo} from "react";
+import { Container, Profiler, Welcome, UserName } from './styles';
+import emojis from '../../utils/emoji';
+
 const MainHeader: React.FC = () => {
+    const emoji = useMemo(() => {
+        const indice = Math.floor( Math.random()* emojis.length);
+        return emojis[indice];
+    },[]);
     return (
         <Container>
             <h1>MainHeader</h1>
+            <Profiler>
+                <Welcome>Olá, {emoji}</Welcome>
+                <UserName>Adria Stephanie</UserName>
+            </Profiler>
         </Container>
     )
 }
