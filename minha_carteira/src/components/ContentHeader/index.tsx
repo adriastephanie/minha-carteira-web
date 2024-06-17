a2ec76fb72af7f3107f164e42e25257d2945d906
adriastephanie <adriastephanie@hotmail.com>
2024-06-17 16:48:22 -0300

    feature:criacao do select

--- minha_carteira/src/components/ContentHeader/index.tsx
+++ minha_carteira/src/components/ContentHeader/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Container, TitleContainer, Controllers } from './styles';
+import SelectInput from "../SelectInput";
 const ContentHeader: React.FC = () => {
+
+    const option = [
+       { value: 'Adria', label: 'Stephanie'},
+       { value: 'Maria', label: 'Bonita'},
+    ]
+    
     return (
         <Container>
             <TitleContainer>
                 <h1>titulo</h1>
             </TitleContainer>
             <Controllers>
-                <button type="button">botao a</button>
-                <button type="button">botao b</button>
-
+                <SelectInput options={option}/>
+                <SelectInput options={option}/>
             </Controllers>
         </Container>
     )
--- minha_carteira/src/pages/Dashboard/index.tsx
+++ minha_carteira/src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "./styles";
 import ContentHeader from "../../components/ContentHeader";
 
+
 const Dashboard: React.FC = () => {
     return (
         <Container>
